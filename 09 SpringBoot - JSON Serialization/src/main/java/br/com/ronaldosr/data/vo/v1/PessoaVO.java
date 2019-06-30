e793031789ee159463fa63d3f38dc65eeab6e5b4
ronaldosr <ronaldosr@users.noreply.github.com>
2019-06-30 19:37:05 -0300

    JSON Serialization
    Adição do @JsonPropertyOrder,  @JsonProperty e @JsonIgnore no data.vo

---
+++ 09 SpringBoot - JSON Serialization/src/main/java/br/com/ronaldosr/data/vo/v1/PessoaVO.java
@@ -0,0 +1,145 @@
+package br.com.ronaldosr.data.vo.v1;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "cpf","primeiro nome", "segundo nome", "sexo", "email"})
+public class PessoaVO implements Serializable {
+
+	private static final long serialVersionUID = -6596573380154259314L;
+	private long id;
+
+	@JsonProperty("primeiro nome")
+	private String primeiroNome;
+	
+	@JsonProperty("segundo nome")
+	private String segundoNome;
+	
+	private String endereco;
+	
+//	@JsonIgnore
+//  A anotação retira o campo, inclusive, do contexto do POST e do PUT!
+	private String sexo;
+	
+	private String cpf;
+	private String email;
+
+	public PessoaVO() {
+	}
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSegundoNome() {
+		return segundoNome;
+	}
+
+	public void setSegundoNome(String segundoNome) {
+		this.segundoNome = segundoNome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((endereco == null) ? 0 : endereco.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((primeiroNome == null) ? 0 : primeiroNome.hashCode());
+		result = prime * result + ((segundoNome == null) ? 0 : segundoNome.hashCode());
+		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaVO other = (PessoaVO) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (endereco == null) {
+			if (other.endereco != null)
+				return false;
+		} else if (!endereco.equals(other.endereco))
+			return false;
+		if (id != other.id)
+			return false;
+		if (primeiroNome == null) {
+			if (other.primeiroNome != null)
+				return false;
+		} else if (!primeiroNome.equals(other.primeiroNome))
+			return false;
+		if (segundoNome == null) {
+			if (other.segundoNome != null)
+				return false;
+		} else if (!segundoNome.equals(other.segundoNome))
+			return false;
+		if (sexo == null) {
+			if (other.sexo != null)
+				return false;
+		} else if (!sexo.equals(other.sexo))
+			return false;
+		return true;
+	}	
+}
---
+++ 09 SpringBoot - JSON Serialization/src/main/java/br/com/ronaldosr/repository/PessoaRepository.java
@@ -0,0 +1,18 @@
+package br.com.ronaldosr.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.ronaldosr.data.model.Pessoa;
+
+@Repository
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
+
+	List<Pessoa> findByCpf(String cpf);
+	
+	Optional<Pessoa> findByEmail(String email);
+
+}
