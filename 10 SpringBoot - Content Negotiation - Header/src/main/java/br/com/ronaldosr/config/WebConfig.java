e21b36a6aafb1a218193dcae617313cfa57c1f82
ronaldosr <ronaldosr@users.noreply.github.com>
2019-07-01 17:59:52 -0300

    10 SpringBoot - Content Negotiation - Header
    Passando Accept application/json ou application/xml no HEADER da requisição

--- 10 SpringBoot - Content Negotiation - Header/src/main/java/br/com/ronaldosr/config/WebConfig.java
+++ 10 SpringBoot - Content Negotiation - Header/src/main/java/br/com/ronaldosr/config/WebConfig.java
@@ -12,35 +12,19 @@ public class WebConfig implements WebMvcConfigurer{
 
 	@Override
 	public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
-		/* Via EXTENSION: 
-		 * localhost:8080/pessoa.xml
-		 * lcoalhost:8080/pessoa.json
-		 * 
+		/*
 		 * configurer.favorParameter(false) .ignoreAcceptHeader(false)
 		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
 		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
 		 */
 		
-		/* Via QUERY PARAM:
-		 * localhost:8080/pessoa?mediaType=xml
-		 * localhost:8080/pessoa?mediaType=json
-		 * 
-		 * configurer.favorParameter(true) .parameterName("mediaType")
-		 * .favorPathExtension(false) .ignoreAcceptHeader(true)
-		 * .useRegisteredExtensionsOnly(false)
-		 * .defaultContentType(MediaType.APPLICATION_JSON) .mediaType("json",
-		 * MediaType.APPLICATION_JSON) .mediaType("xml", MediaType.APPLICATION_XML);
-		 */
-		
-		/*
-		 * Via HEADER
-		 */
-		configurer.favorPathExtension(false)
-		.favorParameter(false)
-		.ignoreAcceptHeader(false)
+		configurer.favorParameter(true)
+		.parameterName("mediaType")
+		.favorPathExtension(false)
+		.ignoreAcceptHeader(true)
 		.useRegisteredExtensionsOnly(false)
 		.defaultContentType(MediaType.APPLICATION_JSON)
 		.mediaType("json", MediaType.APPLICATION_JSON)
-		.mediaType("xml", MediaType.APPLICATION_XML);		 
+		.mediaType("xml", MediaType.APPLICATION_XML);
 	}
 }
